b8818b8dfbf74e29d76dc834f09e73f1223c28b5
knick-knack <knick-knack@4c572d4d-470c-43f1-b6b5-35090c3a2e0e>
2016-04-30 19:51:27 +0000

    src project of implementation of almost all features

---
+++ csharp/Inventor_MikitinMaxim_Khm/deploy/test/Pol.cs
@@ -0,0 +1,37 @@
+﻿using Inventor;
+
+namespace InvAddIn
+{
+    internal class Pol: Section
+    {
+        protected int Number_of_Edges;
+        protected bool is_Inscribed;
+
+        public Pol()
+        {
+            Number_of_Edges = 0;
+            is_Inscribed = true;
+        }
+
+        public Pol(double length, double diametr, int edges, bool inscr )
+        {
+            Radius = 0.5 * diametr;
+            Length = length;
+            Number_of_Edges = edges;
+            is_Inscribed = inscr;
+        }
+
+
+        internal override void Create_BR(TransientGeometry TG, ref PlanarSketch sketch, EdgeCollection eColl, ref Face B_face, ref Face E_face, ref PartComponentDefinition partDef)
+        {
+            sketch.SketchLines.AddAsPolygon(Number_of_Edges, TG.CreatePoint2d(), TG.CreatePoint2d(Radius), is_Inscribed);
+            Profile profile = sketch.Profiles.AddForSolid();
+            ExtrudeDefinition extrude = partDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(profile, PartFeatureOperationEnum.kNewBodyOperation);
+            extrude.SetDistanceExtent(Length, PartFeatureExtentDirectionEnum.kPositiveExtentDirection);
+            ExtrudeFeature extrudeFeature = partDef.Features.ExtrudeFeatures.Add(extrude);
+            sketch = partDef.Sketches.Add(extrudeFeature.EndFaces[1]);
+            Start_face = extrudeFeature.StartFaces[1];
+            End_face = extrudeFeature.EndFaces[1];
+        }
+    }
+}
---
+++ csharp/Inventor_MikitinMaxim_Khm/deploy/test/chamf.cs
@@ -0,0 +1,43 @@
+﻿using Inventor;
+
+namespace InvAddIn
+{
+    public class chamf: Create
+    {
+        protected double Distance;
+        protected char Side;
+
+        internal chamf()
+        {
+            Distance = 0;
+            Side = 'r';
+        } 
+
+        internal chamf(double distance, char side)
+        {
+            Distance = distance;
+            Side = side;
+        }
+
+
+        internal override void Create_BR(TransientGeometry TG, ref PlanarSketch sketch, EdgeCollection eColl, ref Face B_face, ref Face E_face, ref PartComponentDefinition partDef)
+        {
+            ChamferFeature chamf_Feature;
+            switch (Side)
+            {
+                case ('r'):
+                    foreach (Edge e in B_face.Edges)
+                        eColl.Add(e);
+                    chamf_Feature = partDef.Features.ChamferFeatures.AddUsingDistance(eColl, Distance);
+                    break;
+                case ('l'):
+                    foreach (Edge e in E_face.Edges)
+                        eColl.Add(e);
+                    chamf_Feature = partDef.Features.ChamferFeatures.AddUsingDistance(eColl, Distance);
+                    break;
+            }
+            
+        }
+        
+    }
+}
